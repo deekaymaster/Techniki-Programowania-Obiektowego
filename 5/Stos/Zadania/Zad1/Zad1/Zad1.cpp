73bf7a471104f1007159c9a3d721d9e4abee8d15
Dawid Kogut <kogutdawid@gmail.com>
2024-04-18 16:14:16 +0200

    stos

---
+++ 5/Stos/Zadania/Zad1/Zad1/Zad1.cpp
@@ -0,0 +1,43 @@
+﻿// Odwróć kolejność elementów w tablicy z pomocą stosu
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    const int size = 10;
+    int array[size];
+
+    // Inicjalizacja generatora liczb pseudolosowych
+    srand(time(nullptr));
+
+    // Losowanie i wypełnianie tablicy
+    for (int i = 0; i < size; ++i) {
+        array[i] = rand() % 100; // Losowanie liczby od 0 do 99
+    }
+
+    //Wyświetlanie elementów tablicy
+    cout << "Elementy tablicy przed zamiana kolejnosci:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << endl;
+    }
+
+    stack<int> myStack;
+
+    for (int i = 0; i < size; ++i) {
+        myStack.push(array[i]);
+    }
+
+    for (int i = 0; i < size; ++i) {
+        array[i] = myStack.top();
+        myStack.pop();
+    }
+
+    //Wyświetlanie elementów tablicy
+    cout << "Elementy tablicy po zamianie kolejnosci:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << endl;
+    }
+
+}
---
+++ 5/Stos/Zadania/Zad2/Zad2/Zad2.cpp
@@ -0,0 +1,70 @@
+﻿//Napisz program, który będzie sprawdzał, czy nawiasy w danym ciągu są poprawnie
+//sparowane.Program będzie korzystał ze stosu do sprawdzenia równoważności
+//nawiasów.
+//Instrukcje:
+//a.Utwórz klasę BracketChecker, która będzie zawierała metodę checkBrackets,
+//która przyjmie jako argument ciąg znaków(np.std::string), a następnie zwróci
+//true, jeśli nawiasy w ciągu są poprawnie sparowane, lub false, jeśli nie.
+//b.Wewnątrz metody checkBrackets utwórz pusty stos(wykorzystując
+//    std::stack<char>).
+//    c.Przejdź po każdym znaku w ciągu wejściowym.
+//    d.Jeśli napotkasz otwierający nawias((, [, { ), dodaj go na stos.
+//    e.Jeśli napotkasz zamykający nawias(),], }), sprawdź, czy stos nie jest pusty.Jeśli
+//    jest pusty, zwróć false, ponieważ znaleziono zamykający nawias bez
+//    odpowiadającego mu otwierającego nawiasu.
+//    f.Jeśli stos nie jest pusty, pobierz element ze stosu i sprawdź, czy odpowiada on
+//    otwierającemu nawiasowi aktualnego zamykającego nawiasu.Jeśli nie
+//    odpowiada, zwróć false.
+//    g.Po zakończeniu iteracji po wszystkich znakach, sprawdź, czy stos jest pusty.
+//    Jeśli nie jest, zwróć false, ponieważ znaleziono więcej otwierających nawiasów
+//    niż zamykających.
+//    h.Jeśli wszystkie nawiasy są poprawnie sparowane i stos jest pusty, zwróć true.
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class BracketChecker {
+public:
+    static bool checkBrackets(string& str) {
+        stack<char> myStack;
+
+        for (char character : str) {
+            if (character == '(' || character == '[' || character == '{') {
+                myStack.push(character);
+            }
+            else if (character == ')' || character == ']' || character == '}') {
+                if (myStack.empty()) {
+                    return false;
+                }
+                else {
+                    char open = myStack.top();
+                    myStack.pop();
+                    if ((character == ')' && open != '(') ||
+                        (character == ']' && open != '[') ||
+                        (character == '}' && open != '{')) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return myStack.empty(); // Sprawdzamy, czy stos jest pusty (wszystkie nawiasy są sparowane)
+    }
+};
+
+int main()
+{
+    std::string test1 = "(){}[]"; // Poprawnie sparowane nawiasy
+    std::string test2 = "([]{})"; // Poprawnie sparowane nawiasy
+    std::string test3 = "({)}";   // Błędnie sparowane nawiasy
+    std::string test4 = "(((";    // Błędnie sparowane nawiasy
+    std::string test5 = ")}{(";   // Błędnie sparowane nawiasy
+
+    std::cout << "Test 1: " << (BracketChecker::checkBrackets(test1) ? "Poprawne" : "Bledne") << std::endl;
+    std::cout << "Test 2: " << (BracketChecker::checkBrackets(test2) ? "Poprawne" : "Bledne") << std::endl;
+    std::cout << "Test 3: " << (BracketChecker::checkBrackets(test3) ? "Poprawne" : "Bledne") << std::endl;
+    std::cout << "Test 4: " << (BracketChecker::checkBrackets(test4) ? "Poprawne" : "Bledne") << std::endl;
+    std::cout << "Test 5: " << (BracketChecker::checkBrackets(test5) ? "Poprawne" : "Bledne") << std::endl;
+
+    return 0;
+}
